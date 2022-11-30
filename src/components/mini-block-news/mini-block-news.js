264af41d9738e3a0f7441f6991237685e13091b7
lvapl <clashgoglvap@gmail.com>
2022-11-30 11:13:56 +0300

    linkk -> link In mini-block-news component

--- src/components/mini-block-news/mini-block-news.js
+++ src/components/mini-block-news/mini-block-news.js
@@ -4,8 +4,8 @@ import NewsCard from "../news-card/news-card";
 function MiniBlockNews () {
     return (
         <div className="mini-block-news">
-            <NewsCard linkk="/src/images/NewsImage1.png" caption="News 1" description="dsa"/>
-            <NewsCard linkk="/src/images/NewsImage2.png" caption="News 2" description="das"/>
+            <NewsCard link="/src/images/NewsImage1.png" caption="News 1" description="dsa"/>
+            <NewsCard link="/src/images/NewsImage2.png" caption="News 2" description="das"/>
         </div>
     );
 }
